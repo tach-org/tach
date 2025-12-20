004e01733605727b558c633ba15039bc7669015b
Ken Kawamoto <kentaro.kawamoto@gmail.com>
2025-12-20 21:14:26 +0900

    disable ANSI color

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -9,3 +9,14 @@ import pytest
 def example_dir() -> Path:
     current_dir = Path(__file__).parent
     return current_dir / "example"
+
+
+@pytest.fixture(autouse=True, scope="function")
+def no_color(monkeypatch):
+    # According to https://bixense.com/clicolors/, NO_COLOR=1 should be enough.
+    # "console", however, does not respect "NO_COLOR" as of this writing,
+    # and requires CLICOLOR_FORCE being unset.
+    monkeypatch.setenv("NO_COLOR", "1")
+    monkeypatch.delenv("CLICOLOR_FORCE", raising=False)
+
+    yield
